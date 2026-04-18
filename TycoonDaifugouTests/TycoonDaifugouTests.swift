087c820f7ea505eea5c8ec89960f716380773289
Sebi Torres <sebitorres88@gmail.com>
2026-04-18 14:10:25 -0700

    chore: add Xcode project, workspace, and full CI

---
+++ TycoonDaifugouTests/TycoonDaifugouTests.swift
@@ -0,0 +1,19 @@
+//
+//  TycoonDaifugouTests.swift
+//  TycoonDaifugouTests
+//
+//  Created by Sebi Torres on 4/18/26.
+//
+
+import Testing
+@testable import TycoonDaifugou
+
+struct TycoonDaifugouTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
